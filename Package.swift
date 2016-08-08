037b3ee0af416922925bea3436b638b30fdd91c5
Ben Murphy <ben@constellation.software>
2016-08-08 13:37:23 -0400

    Installing Vapor

---
+++ Package.swift
@@ -0,0 +1,8 @@
+import PackageDescription
+
+let package = Package(
+    name: "EttaServer",
+    dependencies: [
+        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 0, minor: 16)
+    ]
+)
